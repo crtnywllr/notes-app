a48ccffbb57f8174e2e99800daef943866781bf5
crtnywllr <courtneycmeadows@gmail.com>
2016-06-18 13:54:12 -0600

    fixed notes display
    Pulls from FE array instead of off db call

--- js/clients.js
+++ js/clients.js
@@ -134,6 +134,7 @@ $(document).ready(function () {
         $("#clientNameDisplay").text(clientName);
         //Send id to display function for use in filtering notes
         singleClientDisplay(clientKey);
+        noteDisplay(clientKey);
         //Update UI
         $("#allClients").hide();
         $("#singleClient").show();
@@ -147,74 +148,6 @@ $(document).ready(function () {
     })
 
     // ---------------- Managing Notes ------------
-
-    //Display notes in table on page load
-    function singleClientDisplay(key) {
-        console.log(key)
-        var details;
-        var currentNotes = notesFromDatabase.filter(function (note) {
-            return (note.key === key)
-        })
-        notesTable.rows().remove().draw();
-        currentNotes.forEach(function (note) {
-            //console.log(note.title)
-            details = [note.title, note.date, note.keywords, note.paid, (note.datePaid || null)]
-            notesTable.row.add(details).draw();
-
-        })
-
-        //console.log(key)
-        //Loop through all notes and display only the ones that match the client key
-        //notesTable.rows().remove();
-        /*database.ref("notes/").once("value", function (snapshot) {
-            snapshot.forEach(function (note) {
-                //console.log("note key ", note.val().key)
-                //console.log("client key ", key)
-
-                if (note.val().key === key) {
-                    notesTable.row.add([note.val().title, note.getKey(), note.val().date, note.val().keywords, note.val().paid, (note.val().datePaid || "")])
-                        .draw();
-                    updateNotesArray(note.val(), note.getKey())
-                } else {
-                    notesTable.rows().remove();
-                };
-            });
-        });*/
-    };
-
-    //Update notes table after add
-    function addNoteToTable(key, date, title, keywords, paid, datePaid) {
-        var noteKey;
-        var note;
-        var noteId;
-        database.ref("notes/").on("child_added", function (snapshot) {
-                noteId = snapshot.getKey();
-                note = snapshot.val()
-                noteKey = snapshot.val().key
-            })
-            //console.log(key);
-            //console.log(noteKey)
-            //console.log(noteId)
-        if (noteKey === key)
-            updateNotesArray(note, noteKey)
-        notesTable.row.add([title, noteId, date, keywords, paid, (datePaid || "")])
-            .draw();
-    }
-
-    //Add notes to database
-    function addNotes(key, date, title, keywords, notes, paid, datePaid, paySource) {
-        database.ref("notes/").push({
-            key: key,
-            date: date,
-            title: title,
-            keywords: keywords,
-            notes: notes,
-            paid: paid,
-            datePaid: datePaid,
-            paySource: paySource
-        });
-        addNoteToTable(key, date, title, keywords, paid, datePaid)
-    }
 
     //Handle noteForm submission
     $("#noteForm").submit(function (event) {
@@ -231,78 +164,111 @@ $(document).ready(function () {
         //Send info to be added to database
         addNotes(key, date, title, keywords, notes, paid, datePaid, paySource);
         //Update UI
-
         $("#notesModal").modal("hide");
     });
 
-    //Add each note to notes array
-    function updateNotesArray(note, noteId) {
-        if (notes === undefined) {
-            notes = [];
-            notes.push({
-                note: note,
-                id: noteId
-            });
+    //Add notes to database
+    function addNotes(key, date, title, keywords, notes, paid, datePaid, paySource) {
+        database.ref("notes/").push({
+            key: key,
+            date: date,
+            title: title,
+            keywords: keywords,
+            notes: notes,
+            paid: paid,
+            datePaid: datePaid,
+            paySource: paySource
+        });
+        singleClientDisplay(key);
+        noteDisplay(key);
+    }
+
+    //Display notes in table
+    function singleClientDisplay(key) {
+        //console.log(key)
+        var currentNotes = notesFromDatabase.filter(function (note) {
+            return (note.key === key)
+        })
+        notesTable.rows().remove().draw();
+        currentNotes.forEach(function (note) {
+            //console.log(note.title)
+            var details = [note.title, note.date, note.keywords, note.paid, (note.datePaid || null)]
+            notesTable.row.add(details).draw();
+        })
+    };
+
+    //Display note content in slideshow
+    function noteDisplay(key) {
+        var currentKey = key
+        console.log("key1", currentKey)
+        if (notesFromDatabase.length === 0) {
+            $("#noteTitle").html(null)
+            $("#noteContent").html("No notes added yet.")
+
         } else {
-            notes.forEach(function (existingNote) {
-                //console.log(noteId)
-                //console.log(existingNote.id)
-                if (noteId === existingNote.id) {
-                    return;
-                } else {
-                    notes.push({
-                        note: note,
-                        id: noteId
+            console.log("key2", currentKey)
+            var currentNotes = notesFromDatabase.filter(function (note) {
+                return (note.key === key)
+            })
+            if (currentNotes.length === 0) {
+                //console.log("key3", key)
+                $("#noteTitle").html(null)
+                $("#noteContent").html("No notes added yet.")
+            } else {
+                console.log("key4", currentKey)
+                var i = (currentNotes.length - 1)
+                    //console.log(i)
+                $("#noteTitle").html(currentNotes[i].title || null)
+                $("#noteContent").html(currentNotes[i].notes || "No notes added yet.")
+                    //scroll through notes array
+                    //!!!!scroll issue when # notes < the prev one viewed
+                $("#decrement").click(function () {
+                    console.log("decKey", key);
+                    console.log("decKey2", currentKey);
+                    var decNotes = currentNotes.filter(function (note) {
+                        return note.key == key
                     });
-                }
-            })
-
+                    //console.log("decNotes", decNotes)
+                    if (decNotes.length === 0) {
+                        return;
+                    } else if (i > 0) {
+                        //console.log("decrement ", currentNotes)
+                        i--;
+                        $("#noteTitle").html(decNotes[i].title)
+                        $("#noteContent").html(decNotes[i].notes)
+                    }
+                });
+                $("#increment").click(function () {
+                    console.log(currentNotes)
+                    if (currentNotes.length === 0) {
+                        return
+                    } else if (i < (currentNotes.length - 1)) {
+                        //console.log("increment ", currentNotes)
+                        i++;
+                        $("#noteTitle").html(currentNotes[i].title)
+                        $("#noteContent").html(currentNotes[i].notes)
+                    }
+                });
+                //change display on click
+                //!!!!!need way to make the click local to link, not whole row
+                $('#notesTable tbody').on('click', 'tr', function () {
+                    //get table row data
+                    var data = notesTable.row(this).data();
+                    var title = data[0];
+                    var keywords = data[2];
+                    //compare data to each note in array
+                    currentNotes.forEach(function (note) {
+                        if (note.title === title && note.keywords === keywords) {
+                            //if data matches, find index of note and display content
+                            i = ($.inArray(note, currentNotes))
+                            $("#noteTitle").html(currentNotes[i].title)
+                            $("#noteContent").html(currentNotes[i].notes)
+                        }
+                    });
+                });
+            }
         }
-        //console.log(notes)
-        //noteDisplay();
-    }
-
-    //Display notes from notes array
-    function noteDisplay() {
-        //console.log(notes.length)
-        var i = (notes.length - 1)
-            //console.log(i)
-        $("#noteTitle").html(notes[i].title || null)
-        $("#noteContent").html(notes[i].notes || "No notes added yet.")
-            //scroll through notes array
-        $("#decrement").click(function () {
-            if (i > 0) {
-                i--;
-                $("#noteTitle").html(notes[i].title)
-                $("#noteContent").html(notes[i].notes)
-            }
-        });
-        $("#increment").click(function () {
-            if (i < (notes.length - 1)) {
-                i++;
-                $("#noteTitle").html(notes[i].title)
-                $("#noteContent").html(notes[i].notes)
-            }
-        });
-        //change display on click
-        //!!!!!need way to make the click local to link, not whole row
-        $('#notesTable tbody').on('click', 'tr', function () {
-            //get table row data
-            var data = notesTable.row(this).data();
-            var title = data[0];
-            var keywords = data[3];
-            //compare data to each note in array
-            notes.forEach(function (note) {
-                if (note.title === title && note.keywords === keywords) {
-                    //if data matches, find index of note and display content
-                    i = ($.inArray(note, notes))
-                    $("#noteTitle").html(notes[i].title)
-                    $("#noteContent").html(notes[i].notes)
-                }
-            })
-        });
-    }
-
+    };
 });
 
 
